d0c19fa684cef4680ea0d905b88eeb38de385b5b
Raja Saputera - <raja.it@smafg.sch.id>
2023-06-06 14:49:15 +0700

    Update data table

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const xss = require('xss-clean');
 
-const User = require('./models/userModel');
 const userRouter = require('./routes/userRoutes');
 const handicraftRouter = require('./routes/handicraftRoutes');
 const AppError = require('./utils/appError');
@@ -51,36 +50,15 @@ app.use('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 const sequelize = require('./utils/database');
-const Handicraft = require('./models/handicraftModel');
 
-const sync = async () => await sequelize.sync({ force: true });
-sync().then(() => {
-  User.create({
-    email: 'test@test.com',
-    password: '123456',
-    name: 'neo ges',
+const sync = async () => await sequelize.sync();
+sync()
+  .then(() => {
+    console.log('Database synced successfully');
+  })
+  .catch((error) => {
+    console.error('Error syncing database:', error);
   });
-  User.create({
-    email: 'test2@test.com',
-    password: '123456',
-    name: 'celeb_neo',
-  });
-  Handicraft.create({
-    name: 'Celengan Botol Plastik',
-    description:
-      'Celengan dari botol bekas air mineral adalah salah satu contoh kerajinan dari botol plastik yang paling populer karena mudah dibuat. Anda bisa mengajak si kecil selama prosesnya agar lebih seru.',
-    photo_url:
-      'https://storage.googleapis.com/rewaste-bucket-capstone/imgdummy.jpg',
-    steps: [
-      'Siapkan botol plastik bekas ukuran bebas.',
-      'Buat lubang koin kira-kira 3 cm di bagian tengah botol. Lubang ini berfungsi sebagai tempat memasukkan koin ataupun uang kertas.',
-      'Cat botol sesuai kreasi.',
-      'Hias botol hingga menjadi celengan lucu. Anda bisa membuatnya menjadi hewan dengan menambahkan gambar hidung di bagian tutup botol, mata, telinga, kaki dan buntut di bagian belakang.',
-      'Celengan dari botol bekas air mineral pun siap dipakai.',
-    ],
-    tags: ['plastic', 'bottle', 'paper'],
-  });
-});
 
 app.use(globalErrorHandler);
 
